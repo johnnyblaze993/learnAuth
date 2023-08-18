8d12450b92c39895e7263234a7cad1485813c923
Jonathan Alva <john.alvarez.93@gmail.com>
2023-08-17 22:10:19 -0500

    added test controller and db connection

---
+++ src/main/java/com/roxy/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package com.roxy.controllers;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello")
+public class HelloController {
+
+    @Get("/")
+    public String index() {
+        return "Hello World!";
+    }
+
+}
